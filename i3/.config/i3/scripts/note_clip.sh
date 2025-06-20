247a97d35353d42649161192b0370f165f1775ff
Otavio <otavio.dittrich@impa.br>
2025-06-20 14:00:22 -0300

    feat(nvim_note_clip): Remove trailing space

--- i3/.config/i3/scripts/note_clip.sh
+++ i3/.config/i3/scripts/note_clip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-tmpfile=$(mktemp /tmp/noteXXXXXX.txt)
+tmpfile=$(mktemp /tmp/noteXXXXXX.tex)
 alacritty --class nvimfloat -e $HOME/bin/nvim.appimage "$tmpfile"
 
 # Copy to clipboard
-xclip -selection clipboard < "$tmpfile"
+head -c -1 "$tmpfile" | xclip -selection clipboard
 
 # Wait for window to lose focus (to avoid pasting into the terminal itself)
 sleep 0.5
--- i3/.config/i3/scripts/nvim_edit_clip.sh
+++ i3/.config/i3/scripts/nvim_edit_clip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tmpfile=$(mktemp /tmp/nvimeditXXXXXX.txt)
+tmpfile=$(mktemp /tmp/nvimeditXXXXXX.tex)
 
 # Simulate Ctrl+C to copy selected text
 sleep 0.1  # Wait for clipboard to update
@@ -15,7 +15,7 @@ xclip -o -selection clipboard > "$tmpfile"
 alacritty --class nvimfloat -e $HOME/bin/nvim.appimage "$tmpfile"
 
 # After editing, copy updated content back to clipboard
-xclip -selection clipboard < "$tmpfile"
+head -c -1 "$tmpfile" | xclip -selection clipboard
 sleep 0.2
 
 # Simulate Ctrl+V to paste it back
