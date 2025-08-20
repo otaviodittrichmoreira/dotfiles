9d0d81fcdd3c3031e1589da3a8726d35bf616053
Otavio <otavio.dittrich@impa.br>
2025-08-19 21:47:28 -0300

    fixbug(tmux): g4f-command now doesn't disappears with the quotes

--- tmux/scripts/g4f-command.sh
+++ tmux/scripts/g4f-command.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-options="Copiar"
-preprompt="Answer the question with options of one-line bash code and its explanation right above
+preprompt="Answer the question with options of one-line bash code and its explanation right above:
+
 "
 
 read -p "Enter Prompt: " query
-# query="How to list disks in linux?"
 
-echo "Prompt: $query" & g4f "$preprompt $query" | sed -n '/^```/,/^```/p' | sed '/^```/d' | fzf | xargs -I {} sh -c "tmux send-keys \"{}\""
+echo "Prompt: $query" & g4f "$preprompt $query" | sed -n '/^```/,/^```/p' | sed '/^```/d' | fzf | sed -e 's/"/\\\"/g' -e "s/'/\\\'/g" | xargs -I {} tmux send-keys "{}"
