930182e068dff803ecf83c329c33c5212f2f65fc
Otavio <otavio.dittrich@impa.br>
2025-08-20 21:27:20 -0300

    bugfix(tmux): g4f-command.sh now saves history and uses it

--- tmux/g4f/g4f-command.sh
+++ tmux/g4f/g4f-command.sh
@@ -3,25 +3,21 @@
 preprompt=$(cat ~/dotfiles/tmux/g4f/preprompt.txt)
 
 read -p "Enter Prompt: " prompt
-# prompt="Copy all files from folder bigfolder to bigfolder2 with progress bar and ETA"
 
 # Check if prompt is in history and has an answer
 prompt_in_history=$(cat ~/dotfiles/tmux/g4f/history.json | jq --arg q "$prompt" 'has(($q)) and .[($q)] != ""')
 
-# Save prompt in history regardless
-cat ~/dotfiles/tmux/g4f/history.json | jq --arg q "$prompt" '. + { ($q): ""}' > ~/dotfiles/tmux/g4f/tmp_history.json
-mv "$g4f_path"tmp_history.json ~/dotfiles/tmux/g4f/history.json
-
 # If prompt was on history, return the previous answer
 if [[ $prompt_in_history == "true" ]]; then
-    answer=$(cat ~/dotfiles/tmux/g4f/history.json | jq --arg q "$prompt" '.[($q)]')
+    answer=$(cat ~/dotfiles/tmux/g4f/history.json | jq -r --arg q "$prompt" '.[($q)]')
+
 else
     answer=$(python ~/dotfiles/tmux/g4f/g4f-runner.py -s "$preprompt" -u "$prompt")
+
+    # Save answer in history
+    cat ~/dotfiles/tmux/g4f/history.json | jq --arg q "$prompt" --arg a "$answer" '.[ $q ] = $a' > ~/dotfiles/tmux/g4f/tmp_history.json
+    mv ~/dotfiles/tmux/g4f/tmp_history.json ~/dotfiles/tmux/g4f/history.json
 fi
 
-# Save answer in history
-cat ~/dotfiles/tmux/g4f/history.json | jq --arg q "$prompt" --arg a "$(cat ~/dotfiles/tmux/g4f/tmp_answer.md)" '.[($q)] = ($a)' > ~/dotfiles/tmux/g4f/tmp_history.json
-mv "$g4f_path"tmp_history.json ~/dotfiles/tmux/g4f/history.json
-
 # Process commands and pipe into fzf and send-keys to tmux pane
-cat ~/dotfiles/tmux/g4f/tmp_answer.md | ~/dotfiles/tmux/g4f/preview-command.sh | sed -e 's/"/\\\"/g' -e "s/'/\\\'/g" | xargs -I {} tmux send-keys "{}"
+printf '%s\n' "$answer" | ~/dotfiles/tmux/g4f/preview-command.sh | sed -e 's/"/\\\"/g' -e "s/'/\\\'/g" | xargs -I {} tmux send-keys "{}"
